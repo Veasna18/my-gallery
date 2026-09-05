a8f2a0713ce10b7baced4c12b2368a53cb74ed85
SNA <omveasna17@gmail.com>
2026-09-05 14:37:21 +0700

    Fix base path in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/my-gallery/',
+  base: process.env.VERCEL ? '/' : '/my-gallery/',
   plugins: [react()],
   resolve: {
     alias: {
